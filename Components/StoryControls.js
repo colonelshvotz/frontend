c0d9b991752b906573d1a89450a0187145f9ceed
colonelshvotz <ads238@gmail.com>
2025-04-20 12:07:33 -0700

    Update StoryControls.js
    Changed button from "Manually Save Chapter" to "Save Chapter"

--- Components/StoryControls.js
+++ Components/StoryControls.js
@@ -81,7 +81,7 @@ export default function StoryControls({
         onClick={handleManualChapterSave}
         className="bg-orange-600 hover:bg-orange-700 text-white py-2 px-4 rounded mt-4"
       >
-        🧪 Manually Save Chapter
+        🧪 Save Chapter
       </button>
 
       <button
