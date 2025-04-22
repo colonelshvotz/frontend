f518779d2195b020f75fda89524837f432bb4472
colonelshvotz <ads238@gmail.com>
2025-04-22 11:38:10 -0700

    Update StoryControls.js
    Change "View Empathy Scores" button to "View Decision Scores" button

--- Components/StoryControls.js
+++ Components/StoryControls.js
@@ -42,7 +42,7 @@ export default function StoryControls({
         onClick={fetchEmpathyScores}
         className="mt-4 bg-indigo-700 hover:bg-indigo-600 text-white px-4 py-2 rounded shadow"
       >
-        🧠 View Empathy Scores
+        🧠 View Decision Scores
       </button>
 
       {showDevPanel && empathyData && (
